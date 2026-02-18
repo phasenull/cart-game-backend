877bd560771f87d0f60d318b28676929ddb44bfb
phasenull <55253035+phasenull@users.noreply.github.com>
2026-02-18 20:34:39 +0300

    drizzle stuff

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,5 @@ import { defineConfig } from "drizzle-kit"
 export default defineConfig({
 	dialect: "sqlite",
 	schema: "./src/schema.ts",
-	driver: "d1-http",
-	dbCredentials: {
-		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-		token: process.env.CLOUDFLARE_D1_TOKEN!
-	}
+	out: "./drizzle"
 })
